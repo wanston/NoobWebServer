a533b0bd87d6e45fa9f1b4b04942396c7bc2bd10
wt <727348402@qq.com>
2019-03-09 16:33:46 +0800

    finished readme.md

--- main.cpp
+++ main.cpp
@@ -11,12 +11,64 @@
 
 using namespace std;
 
-int main() {
+int main(int argc, char *argv[]) {
+    unsigned int threads = 4;
     int port = 8080;
-    unsigned int threads = 5;
     int timeout = 5;
-    LOG.setLogFile("/home/tong/Project/开发/NoobWebServer/test/log.txt");
-    Server s(port, threads, timeout, "./www");
+    std::string logPath = "./log.log";
+    std::string servePath = "./www";
+
+
+
+    // parse args
+    char *p;
+    int opt;
+    const char *str = "n:p:t:l:s:";
+    while ((opt = getopt(argc, argv, str))!= -1)
+    {
+        switch (opt)
+        {
+            case 'n':
+                threads = strtol(optarg, &p, 10);
+                if(threads == 0){
+                    std::cerr << "Invalid argument. -n" << std::endl;
+                    exit(0);
+                }
+                break;
+            case 'p':
+                port = strtol(optarg, &p, 10);
+                if(port <= 0 || port > 65535){
+                    std::cerr << "Invalid option. -p" << std::endl;
+                    exit(0);
+                }
+                break;
+            case 't':
+                timeout = strtol(optarg, &p, 10);
+                if(timeout <= 0){
+                    std::cerr << "Invalid option. -t" << std::endl;
+                    exit(0);
+                }
+                break;
+            case 'l':
+                logPath = optarg;
+                break;
+            case 's':
+                servePath = optarg;
+                break;
+            default:
+                std::cout << "Options:\n"
+                        << "-n thread number\n"
+                        << "-p port\n"
+                        << "-t keep-alive timeout\n"
+                        << "-l log file path\n"
+                        << "-s serve path" << std::endl;
+                exit(0);
+                break;
+        }
+    }
+
+    LOG.setLogFile(logPath);
+    Server s(port, threads, timeout, servePath);
     s.run();
     return 0;
 }
